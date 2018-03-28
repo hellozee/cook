c17ef370a3782bb3775f7b28584683c798ff9acd
hellozee <hellozee@disroot.org>
2018-03-28 21:59:53 +0530

    starting from scratch again

--- parser/lexer.go
+++ parser/lexer.go
@@ -1,124 +1,82 @@
-package parser
+package main
 
 import (
-	"fmt"
-	"strings"
 	"unicode/utf8"
 )
 
-type item struct {
-	typ  itemType
-	pos  int
-	val  string
-	line int
-}
-
-func (i item) String() string {
-	switch {
-	case i.typ == itemEOF:
-		return "EOF"
-	case i.typ == itemError:
-		return i.val
-	case i.typ > itemKeyword:
-		return fmt.Sprintf("<%s>", i.val)
-	case len(i.val) > 10:
-		return fmt.Sprintf("%.10q...", i.val)
-	}
-	return fmt.Sprintf("%q", i.val)
-}
-
-// itemType identifies the type of lex items.
 type itemType int
 
 const (
-	itemError itemType = iota
-	itemEOF
-	itemSpace
-	itemLeftDelim
-	itemRightDelim
-	itemColon
-	itemComma
-	itemString
-	itemKeyword
-	itemFile
-	itemDeps
-	itemCompiler
+	// Literal terminator symbols
+	itemEquals itemType = iota
+	itemSemicolon
+	itemLeftBrace
+	itemrRightBrace
+	//KeyWords
+	itemEntity
 	itemBinary
 	itemName
 	itemStart
 	itemLdFlags
 	itemIncludes
-	itemOtherFlags
+	itemOthers
+	itemFile
+	itemDeps
 )
 
+const eof = -1
+
 var key = map[string]itemType{
-	"file":     itemFile,
-	"deps":     itemDeps,
-	"$":        itemCompiler,
+	"entity":   itemEntity,
 	"binary":   itemBinary,
 	"name":     itemName,
 	"start":    itemStart,
 	"ldflags":  itemLdFlags,
 	"includes": itemIncludes,
-	"others":   itemOtherFlags,
+	"others":   itemOthers,
+	"file":     itemFile,
+	"deps":     itemDeps,
 }
-
-const eof = -1
 
 type lexer struct {
-	name       string
-	input      string
-	leftDelim  string
-	rightDelim string
-	pos        int
-	start      int
-	width      int
-	items      chan item
-	parenDepth int
-	line       int
+	input string
+	pos   int
+	start int
+	width int
+	line  int
 }
 
-type stateFn func(*lexer) stateFn
-
-func (self *lexer) next() rune {
-	if self.pos >= len(self.input) {
-		self.width = 0
+func (lex *lexer) next() rune {
+	if lex.pos >= len(lex.input) {
 		return eof
 	}
 
-	nextRune, runeWidth := utf8.DecodeRuneInString(self.input[self.pos:])
-	self.width = runeWidth
-	self.pos += self.width
+	nextRune, runeWidth := utf8.DecodeRuneInString(lex.input[lex.pos:])
+	lex.width = runeWidth
+	lex.pos += lex.width
+	if isWhiteSpace(nextRune) {
+		return lex.next()
+	}
 	if nextRune == '\n' {
-		self.line++
+		lex.line++
 	}
 	return nextRune
 }
 
-func (self *lexer) peek() rune {
-	nextRune := self.next()
-	self.backup()
+func (lex *lexer) peek() rune {
+	nextRune := lex.next()
+	lex.backup()
 	return nextRune
 }
 
-func (self *lexer) backup() {
-	self.pos -= self.width
-	if self.width == 1 && self.input[self.pos] == '\n' {
-		self.line--
+func (lex *lexer) backup() {
+	lex.pos -= lex.width
+
+	if lex.width == 1 && lex.input[lex.pos] == '\n' {
+		lex.line--
 	}
 }
 
-func (self *lexer) emit(t itemType) {
-	self.items <- item{t, self.start, self.input[self.start:self.pos], self.line}
-
-	switch t {
-	case itemString, itemLeftDelim, itemRightDelim:
-		self.line += strings.Count(self.input[self.start:self.pos], "\n")
-	}
-	self.start = self.pos
-}
-
-func (self *lexer) ignore() {
-	self.line += strings.Count(self.input[self.start:self.pos], "\n")
-	self.start = self.pos
+func isWhiteSpace(currentRune rune) bool {
+	return currentRune == ' ' || currentRune == '\t' || currentRune == '\r'
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,17 @@
-package parser
+package main
+
+import "fmt"
+
+func main() {
+	l := lexer{
+		input: "ab cd",
+		pos:   0,
+		start: 0,
+		width: len("ab cd"),
+		line:  0,
+	}
+
+	for l.peek() != eof {
+		fmt.Println(l.next())
+	}
+}
